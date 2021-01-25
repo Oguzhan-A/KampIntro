437aea3dd6c320d20e5652567482314681b2fb5f
mxcpiv <77890590+mxcpiv@users.noreply.github.com>
2021-01-26 01:36:19 +0300

    New

---
+++ ConstructorWorkbook/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Önce değişkenlerimi tanımlıyorum
+            string _ad, _soyad, _dogumYil, _dogumYeri, _meslek;
+            //Kullanıcıdan verileri girmesini istiyorum ve her bir veriyi ilgili değişkenime atama işlemi yapıyorum
+            Console.WriteLine("Adınızı giriniz: ");
+            _ad = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz: ");
+            _soyad = Console.ReadLine();
+            Console.WriteLine("Doğum yılınızı giriniz: ");
+            _dogumYil = Console.ReadLine();
+            Console.WriteLine("Doğum yerinizi giriniz: ");
+            _dogumYeri = Console.ReadLine();
+            Console.WriteLine("Mesleğinizi giriniz: ");
+            _meslek = Console.ReadLine();
+
+            //Personel classından nesne oluşturuyorum ve ilgili parametrelerini belirtiyorum.
+            Personel prsnl = new Personel(_ad, _soyad, _dogumYil, _dogumYeri, _meslek);
+
+            Console.Read();
+        }
+    }
+    class Personel
+    {
+        //yapıcı metotlarda metot adı ile class adı aynı olur. Burda yapıcı metot içinde ayrıca parametre tanımlaması yaptım. Tüm parametrelerim string, işlem yapmadığım için.
+        public Personel(string ad, string soyad, string dogumYil, string dogumYeri, string meslek)
+        {
+            Console.WriteLine("Ad: " + ad);
+            Console.WriteLine("Soyad: " + soyad);
+            Console.WriteLine("Doğum Yılı: " + dogumYil);
+            Console.WriteLine("Doğum Yeri: " + dogumYeri);
+            Console.WriteLine("Meslek: " + meslek);
+
+        }
+    }
+}
---
+++ Workbook2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Workbook2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer(); 
+            customer1.Age = 23;
+
+            Person person1 = customer1; //Customer customer1 = person1 YAPILAMAZ! 
+            //çünkü Customer:Person (Person asıl diğerleri destekleyici class)
+
+            Console.WriteLine(person1.Age);
+
+            customer1.Age = 35;
+            Console.WriteLine(person1.Age);
+            
+
+
+            
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+    }
+
+    class Customer:Person
+    {
+        public int Id { get; set; }
+
+    }
+
+    class Employee:Person
+    {
+        public int EmpId { get; set; }
+    }
+
+ 
+}
